dd13c40c691c207849ab4440d2700d6dcc837570
ajorquera <ajorquera@users.noreply.github.com>
2022-12-27 18:30:05 +0000

    Deploying to gh-pages from @ ajorquera/fp@8aa957c7183f48d63ed45f389ef39734d2e40a1c 🚀

--- fp.mjs
+++ fp.mjs
@@ -32,13 +32,20 @@ const memoize = (fn, stringify = JSON.stringify) => {
 };
 
 const binaryOp = (operator) => new Function("a", "b", `return a ${operator} b`);
-const identity = (arg) => () => arg;
+const identity = (arg2) => () => arg2;
+const arg = (arg2) => arg2;
 const ifElse = curry((condition, ifFn, elseFn) => (...args2) => condition(...args2) ? ifFn(...args2) : elseFn(...args2));
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const pickRandom = (...args2) => args2[randomNumber(0, args2.length - 1)];
 const map = curryN(2, flip(demethodize(Array.prototype.map)));
 const reduce = curryN(3, flip(demethodize(Array.prototype.reduce)));
-const every = (...fns) => (arg) => demethodize(Array.prototype.every)(fns, (fn) => fn(arg));
-const pipe = (...fns) => (arg) => reduce(arg, (acc, fn) => fn(acc), fns);
-const some = (...fns) => (arg) => fns.some((fn) => fn(arg));
+const every = (...fns) => (arg2) => demethodize(Array.prototype.every)(fns, (fn) => fn(arg2));
+const pipe = (...fns) => (arg2) => reduce(arg2, (acc, fn) => fn(acc), fns);
+const some = (...fns) => (arg2) => fns.some((fn) => fn(arg2));
 const sum = (...args2) => reduce(0, (a, b) => a + b, Array.isArray(args2[0]) ? args2[0] : args2);
 const avg = (...args2) => sum(...args2) / args2.length;
 const compose = flip(pipe);
@@ -46,6 +53,13 @@ const substract = args(reduce(binaryOp("-")));
 const multiply = args(reduce(binaryOp("*")));
 const divide = args(reduce(binaryOp("/")));
 const getProp = curry((attr, obj) => obj[attr]);
+const removeProp = curry((attr, obj) => {
+  const { [attr]: _, ...rest } = obj;
+  return rest;
+});
+const values = Object.values;
+const entries = Object.entries;
+const keys = Object.keys;
 const cloneSpread = (obj) => ({ ...obj });
 const equal = curry((a, b) => a === b);
 const cloneStringify = (obj) => JSON.parse(JSON.stringify(obj));
@@ -58,6 +72,7 @@ const isInfinity = equal(Infinity);
 const isFunction = pipe(typeOf, equal("function"));
 const isArray = Array.isArray;
 const isBoolean = (x) => typeof x === "boolean";
+const isObject = every(pipe(typeOf, equal("object")), not(isArray));
 const isNaN = Number.isNaN;
 const toAbs = (x, abs = Math.abs) => abs(x);
 const isNumber = (x) => !isNaN(Number(x)) && Math.abs(x) !== Infinity;
@@ -66,17 +81,15 @@ const throwError = (msg) => {
   throw msg;
 };
 const stringTemplate = curry((template, obj) => {
-  const keys = Object.keys(obj);
-  const values = Object.values(obj);
-  return new Function(...keys, `return \`${template}\``)(...values);
+  const keys2 = Object.keys(obj);
+  const values2 = Object.values(obj);
+  return new Function(...keys2, `return \`${template}\``)(...values2);
 });
-const ifNotFuncThrowError = ifElse(not(isFunction), (arg) => throwError("No function provided. Receive: " + JSON.stringify(arg)));
+const ifNotFuncThrowError = ifElse(not(isFunction), (arg2) => throwError("No function provided. Receive: " + JSON.stringify(arg2)));
 const curryE = ifNotFuncThrowError(curry);
 const curryNE = ifNotFuncThrowError(curryN);
 const toLocaleStringNumb = curry((lang, options, x) => x.toLocaleString(lang, options));
-const formatLocalNumber = toLocaleStringNumb(navigator.language, {});
-const formatLocalPercentage = toLocaleStringNumb(navigator.language, { style: "percent" });
 const formatLocalCurrency = curry((currency, numb) => toLocaleStringNumb(navigator.language, { style: "currency", currency }, numb));
 
-export { args, avg, binaryOp, cloneSpread, cloneStringify, compose, curry, curryE, curryN, curryNE, demethodize, divide, equal, every, flip, formatLocalCurrency, formatLocalNumber, formatLocalPercentage, getProp, identity, ifElse, ifNotFuncThrowError, isArray, isBoolean, isFunction, isInfinity, isNaN, isNumber, isNumber2, logger, map, memoize, multiply, negate, not, pipe, reduce, some, stringTemplate, substract, sum, throwError, timer, to, toAbs, toBoolean, toLocaleStringNumb, toNumber, toSring, typeOf };
+export { arg, args, avg, binaryOp, cloneSpread, cloneStringify, compose, curry, curryE, curryN, curryNE, demethodize, divide, entries, equal, every, flip, formatLocalCurrency, getProp, identity, ifElse, ifNotFuncThrowError, isArray, isBoolean, isFunction, isInfinity, isNaN, isNumber, isNumber2, isObject, keys, logger, map, memoize, multiply, negate, not, pickRandom, pipe, reduce, removeProp, some, stringTemplate, substract, sum, throwError, timer, to, toAbs, toBoolean, toLocaleStringNumb, toNumber, toSring, typeOf, values };
 //# sourceMappingURL=fp.mjs.map
