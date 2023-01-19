57f54bfd6c59ffba32a200ab51c0e85ca6072966
andres <jorquera.ad@gmail.com>
2023-01-19 13:33:47 +0100

    Adding helpers

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,221 +1,240 @@
 import { not } from '../index';
-import {binaryOp, compose, createLogger, curryE, equal, every, filter, getProp, identity, ifElse,isNumber, map, pipe, reduce, some, stringTemplate, sum, toLocaleCurrency, toLocaleStringNumb} from './utils'
+import {
+  binaryOp,
+  compose,
+  createLogger,
+  curryE,
+  equal,
+  every,
+  filter,
+  getProp,
+  identity,
+  ifElse,
+  isDate,
+  isNumber,
+  map,
+  pipe,
+  reduce,
+  some,
+  stringTemplate,
+  sum,
+  toLocaleCurrency,
+  toLocaleStringNumb,
+} from './utils';
 
 test('reduce', () => {
-    const result = reduce(0, (acc, val) => acc + val, [1,2,3]);
-    expect(result).toBe(6);
+  const result = reduce(0, (acc, val) => acc + val, [1, 2, 3]);
+  expect(result).toBe(6);
 });
-
 
 test('binaryOper', () => {
-    const sum = binaryOp('+');
+  const sum = binaryOp('+');
 
-    expect(sum(1,2)).toBe(3);
-    expect(sum(1,2,5)).not.toBe(8);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2, 5)).not.toBe(8);
 
-    const substract = binaryOp('-');
-    expect(substract(1,2)).toBe(-1);
+  const substract = binaryOp('-');
+  expect(substract(1, 2)).toBe(-1);
 });
-
-
 
 test('sum', () => {
-    expect(sum(1,2)).toBe(3);
-    expect(sum([1,2])).toBe(3);
-    expect(sum(1,2,5)).toBe(8);
-    expect(sum([1,2,5])).toBe(8);
-    expect(sum(100,2,3,4,-5)).toBe(104);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum([1, 2])).toBe(3);
+  expect(sum(1, 2, 5)).toBe(8);
+  expect(sum([1, 2, 5])).toBe(8);
+  expect(sum(100, 2, 3, 4, -5)).toBe(104);
 });
 
-
 test('map', () => {
-    const result = map((val) => val + 1, [1,2,3]);
-    expect(result).toEqual([2,3,4]);
-})
-
+  const result = map((val) => val + 1, [1, 2, 3]);
+  expect(result).toEqual([2, 3, 4]);
+});
 
 test('getProp', () => {
-    const obj = {data: 'my-cooldata', nested: {data: 'my-nested-data'}};
-    const getDataProp = getProp('data');
-    const getNestedDataProp = getProp('nested.data');
+  const obj = { data: 'my-cooldata', nested: { data: 'my-nested-data' } };
+  const getDataProp = getProp('data');
+  const getNestedDataProp = getProp('nested.data');
 
-    expect(getDataProp(obj)).toBe(obj.data);
-    expect(getNestedDataProp(obj)).toBe(obj.nested.data);
-})
-
+  expect(getDataProp(obj)).toBe(obj.data);
+  expect(getNestedDataProp(obj)).toBe(obj.nested.data);
+});
 
 test('isNumber', () => {
-    expect(isNumber(1)).toBe(true);
-    expect(isNumber('1')).toBe(true);
-    expect(isNumber(NaN)).toBe(false);
-    expect(isNumber(Infinity)).toBe(false);
+  expect(isNumber(1)).toBe(true);
+  expect(isNumber('1')).toBe(true);
+  expect(isNumber(NaN)).toBe(false);
+  expect(isNumber(Infinity)).toBe(false);
 });
 
 test('pipe', () => {
-    let result;    
-    
-    result = pipe(
-        map((val) => val + 1),
-        sum
-    )([1,2,3]);
+  let result;
 
-    expect(result).toBe(9);
+  result = pipe(
+    map((val) => val + 1),
+    sum
+  )([1, 2, 3]);
+
+  expect(result).toBe(9);
 });
 
 test('compose', () => {
-    let result;
+  let result;
 
-    result = compose(
-        sum,
-        map((val) => val + 1),
-    )([1,2,3]);
+  result = compose(
+    sum,
+    map((val) => val + 1)
+  )([1, 2, 3]);
 
-    expect(result).toBe(9);
+  expect(result).toBe(9);
 });
 
 const getTrue = identity(true);
 const getFalse = not(getTrue);
 test('every', () => {
-    const allIdentity = every(getTrue, getTrue, getTrue);
-    expect(allIdentity(true)).toBe(true);
+  const allIdentity = every(getTrue, getTrue, getTrue);
+  expect(allIdentity(true)).toBe(true);
 
-    const oneFalse = every(getFalse, getTrue, getTrue);
-    expect(oneFalse(true)).toBe(false);
+  const oneFalse = every(getFalse, getTrue, getTrue);
+  expect(oneFalse(true)).toBe(false);
 });
 
 test('some', () => {
-    const allFalse = some(getFalse, getFalse, getFalse);
-    expect(allFalse(false)).toBe(false);
+  const allFalse = some(getFalse, getFalse, getFalse);
+  expect(allFalse(false)).toBe(false);
 
-    const oneNotIdentity = some(getTrue, getTrue, getTrue, getFalse);
-    expect(oneNotIdentity(false)).toBe(true);
+  const oneNotIdentity = some(getTrue, getTrue, getTrue, getFalse);
+  expect(oneNotIdentity(false)).toBe(true);
 });
 
-test('stringTemplate' , () => {
-    const template = stringTemplate('${greating}, ${name}!');
-    expect(template({name: 'John', greating: 'Hello'})).toBe('Hello, John!');
-    expect(template({name: 'Andres', greating: 'Hey'})).toBe('Hey, Andres!');
+test('stringTemplate', () => {
+  const template = stringTemplate('${greating}, ${name}!');
+  expect(template({ name: 'John', greating: 'Hello' })).toBe('Hello, John!');
+  expect(template({ name: 'Andres', greating: 'Hey' })).toBe('Hey, Andres!');
 });
 
 test('curryE', () => {
-    const fn = (a,b,c) => [a,b,c];
-    let curriedFn;
+  const fn = (a, b, c) => [a, b, c];
+  let curriedFn;
 
-    expect(() => curryE({})).toThrowError('No function provided. Receive: {}');
+  expect(() => curryE({})).toThrowError('No function provided. Receive: {}');
 
-    curriedFn = curryE(fn);
+  curriedFn = curryE(fn);
 
-    const result = curriedFn(1)(2)(3);
-    expect(result).toEqual([1,2,3]);
-})
-
-test('ifElse', () => {
-    const trueMock = jest.fn();
-    const falseMock = jest.fn();
-
-    const checkArg = ifElse(equal(true), trueMock, falseMock);
-
-    checkArg(true);
-    expect(trueMock).toBeCalledTimes(1);
-    expect(falseMock).toBeCalledTimes(0);
-
-    checkArg(false);
-
-    expect(trueMock).toBeCalledTimes(1);
-    expect(falseMock).toBeCalledTimes(1);
+  const result = curriedFn(1)(2)(3);
+  expect(result).toEqual([1, 2, 3]);
 });
 
-test('filter', () => {   
-    const result = filter((val) => val > 1, [1,2,3]);
-    expect(result).toEqual([2,3]);
+test('ifElse', () => {
+  const trueMock = jest.fn();
+  const falseMock = jest.fn();
+
+  const checkArg = ifElse(equal(true), trueMock, falseMock);
+
+  checkArg(true);
+  expect(trueMock).toBeCalledTimes(1);
+  expect(falseMock).toBeCalledTimes(0);
+
+  checkArg(false);
+
+  expect(trueMock).toBeCalledTimes(1);
+  expect(falseMock).toBeCalledTimes(1);
+});
+
+test('filter', () => {
+  const result = filter((val) => val > 1, [1, 2, 3]);
+  expect(result).toEqual([2, 3]);
 });
 
 describe('createLogger', () => {
-    const strToLog = 'test';
-    jest.spyOn(console, 'log').mockImplementation(() => {});
-    jest.spyOn(console, 'warn').mockImplementation(() => {});
-    jest.spyOn(console, 'error').mockImplementation(() => {});
-    
-    const mockConsole = {
-        log: jest.fn(),
-        warn: jest.fn(),
-        error: jest.fn(),
-        info: jest.fn(),
-        debug: jest.fn()
-    };
+  const strToLog = 'test';
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
 
-    beforeEach(() => {
-        (console.log as jest.Mock).mockClear();
-        (console.warn  as jest.Mock).mockClear();
-        (console.error  as jest.Mock).mockClear();
-    });
+  const mockConsole = {
+    log: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+    info: jest.fn(),
+    debug: jest.fn(),
+  };
 
-    test('should log, warn and error', () => {
-    
-        const log = createLogger('log');
-        const warn = createLogger('warn');
-        const error = createLogger('error');
-    
-        log(strToLog);
-        warn(strToLog);
-        error(strToLog);
-        
-        expect(console.log).toBeCalledTimes(1);
-        expect(console.log).toBeCalledWith(strToLog);
-    
-        expect(console.warn).toBeCalledTimes(1);
-        expect(console.warn).toBeCalledWith(strToLog);
-    
-        expect(console.error).toBeCalledTimes(1);
-        expect(console.error).toBeCalledWith(strToLog);
-    });
+  beforeEach(() => {
+    (console.log as jest.Mock).mockClear();
+    (console.warn as jest.Mock).mockClear();
+    (console.error as jest.Mock).mockClear();
+  });
 
-    test('should log, warn and error with prefix', () => {
-        const prefix = 'prefix';
-        const logPrefix = createLogger('log', prefix);
-    
-        logPrefix(strToLog);
-    
-        expect(console.log).toBeCalledTimes(1);
-        expect(console.log).toBeCalledWith(prefix, strToLog);
-    });
+  test('should log, warn and error', () => {
+    const log = createLogger('log');
+    const warn = createLogger('warn');
+    const error = createLogger('error');
 
+    log(strToLog);
+    warn(strToLog);
+    error(strToLog);
 
-    test('should log, warn and error with custom console', () => {
-        const logConsoleMock = createLogger('log', '', mockConsole);
-        logConsoleMock(strToLog);
-    
-        expect(mockConsole.log).toBeCalledTimes(1);
-        expect(mockConsole.log).toBeCalledWith(strToLog);
-    });
-})
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(strToLog);
+
+    expect(console.warn).toBeCalledTimes(1);
+    expect(console.warn).toBeCalledWith(strToLog);
+
+    expect(console.error).toBeCalledTimes(1);
+    expect(console.error).toBeCalledWith(strToLog);
+  });
+
+  test('should log, warn and error with prefix', () => {
+    const prefix = 'prefix';
+    const logPrefix = createLogger('log', prefix);
+
+    logPrefix(strToLog);
+
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(prefix, strToLog);
+  });
+
+  test('should log, warn and error with custom console', () => {
+    const logConsoleMock = createLogger('log', '', mockConsole);
+    logConsoleMock(strToLog);
+
+    expect(mockConsole.log).toBeCalledTimes(1);
+    expect(mockConsole.log).toBeCalledWith(strToLog);
+  });
+});
 
 test('toLocaleStringNumb', () => {
-    const toESNumber = toLocaleStringNumb('es', {});
+  const toESNumber = toLocaleStringNumb('es', {});
 
-    expect(toESNumber(1000)).toBe('1000');
-    expect(toESNumber(1000000)).toBe('1.000.000');
-    expect(toESNumber(1000000.123)).toBe('1.000.000,123');
+  expect(toESNumber(1000)).toBe('1000');
+  expect(toESNumber(1000000)).toBe('1.000.000');
+  expect(toESNumber(1000000.123)).toBe('1.000.000,123');
 
-    const toUSNumber = toLocaleStringNumb('en-US', {});
+  const toUSNumber = toLocaleStringNumb('en-US', {});
 
-    expect(toUSNumber(1000)).toBe('1,000');
-    expect(toUSNumber(1000000)).toBe('1,000,000');
-    expect(toUSNumber(1000000.1233232)).toBe('1,000,000.123');
-
+  expect(toUSNumber(1000)).toBe('1,000');
+  expect(toUSNumber(1000000)).toBe('1,000,000');
+  expect(toUSNumber(1000000.1233232)).toBe('1,000,000.123');
 });
 
 test('toLocaleCurrency', () => {
-    const toESEuroCurrency = toLocaleCurrency('es', 'EUR');
+  const toESEuroCurrency = toLocaleCurrency('es', 'EUR');
 
-    expect(toESEuroCurrency(1000)).toBe('1000,00 €');
-    expect(toESEuroCurrency(1000000)).toBe('1.000.000,00 €');
-    expect(toESEuroCurrency(1000000.123)).toBe('1.000.000,12 €');
+  expect(toESEuroCurrency(1000)).toBe('1000,00 €');
+  expect(toESEuroCurrency(1000000)).toBe('1.000.000,00 €');
+  expect(toESEuroCurrency(1000000.123)).toBe('1.000.000,12 €');
 
-    const toUSUsdCurrency = toLocaleCurrency('en-US', 'USD');
+  const toUSUsdCurrency = toLocaleCurrency('en-US', 'USD');
 
-    expect(toUSUsdCurrency(1000)).toBe('$1,000.00');
-    expect(toUSUsdCurrency(1000000)).toBe('$1,000,000.00');
-    expect(toUSUsdCurrency(1000000.1233232)).toBe('$1,000,000.12');
+  expect(toUSUsdCurrency(1000)).toBe('$1,000.00');
+  expect(toUSUsdCurrency(1000000)).toBe('$1,000,000.00');
+  expect(toUSUsdCurrency(1000000.1233232)).toBe('$1,000,000.12');
 });
 
+test('isDate', () => {
+  expect(isDate(new Date())).toBe(true);
+  expect(isDate(new Date('sdf'))).toBe(false);
+  expect(isDate('')).toBe(false);
+  expect(isDate(1)).toBe(false);
+  expect(isDate({})).toBe(false);
+});
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,19 @@
-import { args, curry, curryN, demethodize, flip, not } from "../HOF/index";
+import { args, curry, curryN, demethodize, flip, not } from '../HOF/index';
 
 export const binaryOp = (operator) => new Function('a', 'b', `return a ${operator} b`);
-export const identity = arg => () => arg;
-export const arg = arg => arg;
-export const ifElse = curry((condition, ifFn, elseFn) => (...args) => condition(...args) ? ifFn(...args) : elseFn(...args));
+export const identity = (arg) => () => arg;
+export const arg = (arg) => arg;
+export const ifElse = curry(
+  (condition, ifFn, elseFn) =>
+    (...args) =>
+      condition(...args) ? ifFn(...args) : elseFn(...args)
+);
 
-function randomNumber(min, max) { 
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export const pickRandom = (...args) => args[randomNumber(0, args.length - 1)];
 
@@ -18,10 +22,19 @@ export const filter = curryN(2, flip(demethodize(Array.prototype.filter)));
 export const reduce = curryN(3, flip(demethodize(Array.prototype.reduce)));
 
 export const flat = demethodize(Array.prototype.flat);
-export const every = (...fns) => arg => demethodize(Array.prototype.every)(fns, fn => fn(arg));
-export const pipe = (...fns) => arg => reduce(arg, (acc, fn) => fn(acc), fns);
-export const some = (...fns) => arg => fns.some(fn => fn(arg));
-export const sum = (...args) => reduce(0, (a,b)=> a + b, Array.isArray(args[0]) ? args[0] : args);                                  
+export const every =
+  (...fns) =>
+  (arg) =>
+    demethodize(Array.prototype.every)(fns, (fn) => fn(arg));
+export const pipe =
+  (...fns) =>
+  (arg) =>
+    reduce(arg, (acc, fn) => fn(acc), fns);
+export const some =
+  (...fns) =>
+  (arg) =>
+    fns.some((fn) => fn(arg));
+export const sum = (...args) => reduce(0, (a, b) => a + b, Array.isArray(args[0]) ? args[0] : args);
 export const avg = (...args) => sum(...args) / args.length;
 export const uniq = (arr) => [...new Set(arr)];
 
@@ -31,23 +44,22 @@ export const multiply = args(reduce(binaryOp('*')));
 export const divide = args(reduce(binaryOp('/')));
 
 export const getProp = curry((path: string, obj) => {
-    const pathArr = path.split('.');
-    return pathArr.reduce((acc, val) => acc === undefined ? acc : acc[val], obj);
+  const pathArr = path.split('.');
+  return pathArr.reduce((acc, val) => (acc === undefined ? acc : acc[val]), obj);
 });
 
-
 export const removeProp = curry((attr, obj) => {
-    const {[attr]: _, ...rest} = obj;
-    return rest;
+  const { [attr]: _, ...rest } = obj;
+  return rest;
 });
 
 export const values = Object.values;
 export const entries = Object.entries;
 export const keys = Object.keys;
 
-export const cloneSpread = (obj) => ({...obj});
-export const cloneStringify = (obj) => JSON.parse(JSON.stringify(obj)); 
-export const equal = curry((a,b) => a === b);
+export const cloneSpread = (obj) => ({ ...obj });
+export const cloneStringify = (obj) => JSON.parse(JSON.stringify(obj));
+export const equal = curry((a, b) => a === b);
 export const typeOf = (x) => typeof x;
 export const to = curry((constr, x) => new constr(x));
 
@@ -58,35 +70,44 @@ export const toSring = to(String);
 export const isInfinity = equal(Infinity);
 export const isFunction = pipe(typeOf, equal('function'));
 export const isArray = Array.isArray;
-export const isBoolean = x => typeof x === 'boolean';
+export const isBoolean = (x) => typeof x === 'boolean';
 export const isObject = every(pipe(typeOf, equal('object')), not(isArray));
 export const isNaN = Number.isNaN;
-export const toAbs = (x, abs=Math.abs) => abs(x)
-export const isNumber = x => !isNaN(Number(x)) && Math.abs(x) !== Infinity;
+export const toAbs = (x, abs = Math.abs) => abs(x);
+export const isNumber = (x) => !isNaN(Number(x)) && Math.abs(x) !== Infinity;
 export const isNumber2 = every(pipe(toNumber, not(isNaN)), pipe(toAbs, not(isInfinity)));
+export const instanceOf = curry((constr, x) => x instanceof constr);
+export const isDate = every(instanceOf(Date), pipe(toNumber, isNumber));
 
-
-export const throwError = (msg) => { throw msg  };
+export const throwError = (msg) => {
+  throw msg;
+};
 
 export const stringTemplate = curry((template, obj) => {
-    const keys = Object.keys(obj);
-    const values = Object.values(obj);
-    return new Function(...keys, `return \`${template}\``)(...values);
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
+  return new Function(...keys, `return \`${template}\``)(...values);
 });
 
-export const ifNotFuncThrowError = ifElse(not(isFunction), (arg) => throwError('No function provided. Receive: ' + JSON.stringify(arg)))
+export const ifNotFuncThrowError = ifElse(not(isFunction), (arg) =>
+  throwError('No function provided. Receive: ' + JSON.stringify(arg))
+);
 
 export const curryE = ifNotFuncThrowError(curry);
 export const curryNE = ifNotFuncThrowError(curryN);
 
 type logLevel = 'log' | 'warn' | 'error' | 'info' | 'debug';
-type console = Pick<Console, logLevel>
-export const createLogger = (name:logLevel,prefix='', cons:console=console) => {
-    return (...args) => prefix ? cons[name](prefix, ...args) : cons[name](...args);
+type console = Pick<Console, logLevel>;
+export const createLogger = (name: logLevel, prefix = '', cons: console = console) => {
+  return (...args) => (prefix ? cons[name](prefix, ...args) : cons[name](...args));
 };
 
-export const toLocaleStringNumb = curry((lang: Intl.LocalesArgument, options: Intl.NumberFormatOptions, x: number) => x.toLocaleString(lang, options));
+export const toLocaleStringNumb = curry((lang: Intl.LocalesArgument, options: Intl.NumberFormatOptions, x: number) =>
+  x.toLocaleString(lang, options)
+);
 
 type currency = 'USD' | 'EUR';
 type lang = 'es' | 'en';
-export const toLocaleCurrency = curry((lang: lang, currency: currency, numb: number) => toLocaleStringNumb(lang, {style: 'currency', currency}, numb));
+export const toLocaleCurrency = curry((lang: lang, currency: currency, numb: number) =>
+  toLocaleStringNumb(lang, { style: 'currency', currency }, numb)
+);
